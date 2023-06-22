717cf4e568ef882b9197a7c0c5a77ac4e60c356b
AKASH <kumarakash11072002@gmail.com>
2023-06-22 16:21:42 +0530

    GofoodMern

---
+++ backend/db.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+// const mongoDbClient = require("mongodb").MongoClient
+const mongoURI = 'mongodb+srv://BringFood:v7k5f38uX55KHNMg@cluster0.vjqj27p.mongodb.net/BringFoodMern?retryWrites=true&w=majority'
+module.exports = function (callback) {
+    mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
+        // mongoDbClient.connect(mongoURI, { useNewUrlParser: true }, async(err, result) => {
+        if (err) console.log("---" + err)
+        else {
+            // var database =
+            console.log("connected to mongo")
+            const foodCollection = await mongoose.connection.db.collection("food_items");
+            foodCollection.find({}).toArray(async function (err, data) {
+                const categoryCollection = await mongoose.connection.db.collection("food_categories");
+                categoryCollection.find({}).toArray(async function (err, Catdata) {
+                    callback(err, data, Catdata);
+
+                })
+            });
+            // listCollections({name: 'food_items'}).toArray(function (err, database) {
+            // });
+            //     module.exports.Collection = database;
+            // });
+        }
+    })
+};
