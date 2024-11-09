21cb9ae90e0db717f72757feae3af143097c0dcb
akash-1107 <2021ugcs040@nitjsr.ac.in>
2024-11-09 16:22:56 +0530

    fixed carousel buttons

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -28,39 +28,46 @@ export default function Home() {
   }, [])
 
   return (
-    <div >
-      <div>
+    < div>
         <Navbar />
-      </div>
-      <div>
-        <div id="carouselExampleFade" className="carousel slide carousel-fade " data-bs-ride="carousel">
-
-          <div className="carousel-inner " id='carousel'>
-            <div class=" carousel-caption  " style={{ zIndex: "9" }}>
-              <div className=" d-flex justify-content-center">  {/* justify-content-center, copy this <form> from navbar for search box */}
-                <input className="form-control me-2 w-75 bg-white text-dark" type="search" placeholder="Search in here..." aria-label="Search" value={search} onChange={(e) => { setSearch(e.target.value) }} />
-                <button className="btn text-white bg-danger" onClick={() => { setSearch('') }}>X</button>
-              </div>
-            </div>
-            <div className="carousel-item active" >
-              <img src="https://source.unsplash.com/random/900x700/?burger" className="d-block w-100  " style={{ filter: "brightness(30%)" }} alt="..." />
-            </div>
-            <div className="carousel-item">
-              <img src="https://source.unsplash.com/random/900x700/?pastry" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
-            </div>
-            <div className="carousel-item">
-              <img src="https://source.unsplash.com/random/900x700/?barbeque" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
+      {/* Carousel */}
+      <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
+        <div className="carousel-inner" id="carousel">
+          <div className="carousel-caption" style={{ zIndex: "9" }}>
+            <div className="d-flex justify-content-center">
+              <input
+                className="form-control me-2 w-75 bg-white text-dark"
+                type="search"
+                placeholder="Search in here..."
+                aria-label="Search"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <button className="btn text-white bg-danger" onClick={() => setSearch('')}>X</button>
             </div>
           </div>
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-          </button>
+
+          {/* Carousel Items */}
+          <div className="carousel-item active">
+            <img src={burger} className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="Burger" />
+          </div>
+          <div className="carousel-item">
+            <img src={dosa} className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="Dosa" />
+          </div>
+          <div className="carousel-item" id = "nam">
+            <img src={pizza} className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="Pizza" />
+          </div>
         </div>
+
+        {/* Carousel Controls */}
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
       <div className='container'> {/* boootstrap is mobile first */}
         {
