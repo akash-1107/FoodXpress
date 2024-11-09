21ef301b658c068fd9934147f9ca08e015b322d6
akash-1107 <2021ugcs040@nitjsr.ac.in>
2024-11-09 14:26:58 +0530

    updated carousel

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://foodxpressmern.netlify.app");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
--- frontend/src/components/Carousel.js
+++ frontend/src/components/Carousel.js
@@ -14,13 +14,16 @@ export default function Carousel() {
                         </form>
                     </div>
                     <div className="carousel-item active" >
-                        <img src="https://source.unsplash.com/random/900x700/?burger" className="d-block w-100  " style={{ filter: "brightness(30%)" }} alt="..." />
+                        <img src="../Images/burger.jpg" className="d-block w-100  " style={{ filter: "brightness(30%)" }} alt="..." />
                     </div>
                     <div className="carousel-item">
-                        <img src="https://source.unsplash.com/random/900x700/?pastry" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
+                        <img src="../Images/biryani.jpeg" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
                     </div>
                     <div className="carousel-item">
-                        <img src="https://source.unsplash.com/random/900x700/?barbeque" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
+                        <img src="../Images/pizza.jpg" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="../Images/dosa.jpeg" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
